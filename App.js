8ccac296d8f1a07798507eea165758dde5bbe31f
Igor Graszkiewicz <igras003@gold.ac.uk>
2021-04-14 21:24:03 +0100

    Database is set up on startup

--- App.js
+++ App.js
@@ -1,18 +1,47 @@
 import'react-native-gesture-handler';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import { Paragraph, Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import theme from './CustomProperties/Themes';
 import {createDefaultTables, testQuery} from "./FileStorage/Database";
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 import MainScreen from './Screens/MainScreen';
 
 export default function App() {
-  //createDefaultTables();
+  const [appReady, setAppReady] = useState(false);
+  const [firstStartup, setFirstStartup] = useState(false);
+
+  useEffect(() => {
+      AsyncStorage.getItem("firstStartup", (error, result) => {
+        if (error){
+          throw error;
+        }
+        if (!result) {
+          // Initiate first time startup sequence
+          AsyncStorage.setItem("firstStartup", JSON.stringify({firstStartup: true}), (error, result) => {
+            if (error) {
+              throw error;
+            }
+            createDefaultTables(()=> {
+              setFirstStartup(true);
+              setAppReady(true);
+            });
+          });
+        }
+        else {
+          setFirstStartup(false);
+          setAppReady(true);
+        }
+      });
+    },
+    []
+  );
+  
 
   return (
       <PaperProvider theme = { theme }>
-          <MainScreen />
+          { appReady? <MainScreen/> : null}
       </PaperProvider>
   );
 }
