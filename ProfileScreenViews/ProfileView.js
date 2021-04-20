66a27b19c9a00b2f08362a940ca2048c67e1acc2
arxu <podoboo2@gmail.com>
2021-04-20 18:37:15 +0100

    testing edit methods

--- ProfileScreenViews/ProfileView.js
+++ ProfileScreenViews/ProfileView.js
@@ -4,7 +4,6 @@ import {Image, StyleSheet, ScrollView, View} from "react-native";
 import {TouchableWithoutFeedback as TWF} from 'react-native-gesture-handler';
 
 import { getAllUsers } from "../FileStorage/Database";
-import ProfileDataTable from '../Components/ProfileDataTable';
 import { disableExpoCliLogging } from 'expo/build/logs/Logs';
 
 var ld = require('lodash');
@@ -17,8 +16,15 @@ export default class ProfileView extends Component {
 
         this.state = {
             profile: [],
-            editMode: false
+            edit: false,
+            title: "Profile"
           };
+
+        this.edited = {
+            username : ""
+
+        };
+          //get user profile
           getAllUsers((error, result) => {
             if (error) {
               console.log(error);
@@ -27,29 +33,6 @@ export default class ProfileView extends Component {
               this.setState({profile: result[0]});
             }
           });
-
-        // this.state = {
-        //     editMode: false,
-        //     editDialogVisible: false,
-        //     editDialogIsNumeric: true,
-        //     editDialogLabel: null,
-        //     editDialogTitle: null,
-        //     currEditedElements: [],
-        //     profile: ld.cloneDeep(props.route.params.data),
-        //     editedProfile: ld.cloneDeep(props.route.params.data)
-        // }
-
-        // this.openEditDialog = (elements, dialogTitle, dialogLabel) => {
-        //     this.setState({
-        //         editDialogIsNumeric: false,
-        //         currEditedElements: elements,
-        //         editDialogTitle: dialogTitle,
-        //         editDialogLabel: dialogLabel  
-        //     }, () => { 
-        //         console.log(this.state.currEditedElements); // Logs array ["username"]
-        //         this.setState({editDialogVisible: true,});
-        //     });
-        // }
     }
     
     render() { 
@@ -57,27 +40,29 @@ export default class ProfileView extends Component {
             <React.Fragment>
                 <Appbar.Header>
                     <Appbar.Content 
-                        title="Profile"
-                        // onPress={ ()=> {
-                        //     this.state.editMode ?
-                        //         this.openEditDialog(["username"], "Profile Name", "Profile Name")
-                        //     : 
-                        //         null;
-                        // }}
+                        title = {this.state.title}
                     />
                     <Appbar.Action 
                         icon={MORE_ICON} 
-                        onPress={ () => { this.setState({editMode: !this.state.editMode})}}
+                        onPress={ () => { this.setState({edit: !this.state.edit})}}
                     />
                 </Appbar.Header>
                 <ScrollView>
                     <React.Fragment>
                         <View style={{paddingLeft: 17, paddingRight: 17}}>
-                            {/* <ProfileDataTable profile={this.state.profile}/> */}
                             <DataTable>
                                 <DataTable.Header>
                                     <DataTable.Title>Name</DataTable.Title>
-                                    <DataTable.Cell>{this.state.profile.username}</DataTable.Cell>
+                                    <DataTable.Cell>{this.state.editing ? (
+                                        this.state.profile.username
+                                    ) : (
+                                        <TextInput type = "text"
+                                        value = {this.state.profile.username}
+                                        ref={node => {
+                                            this.edited.username = node;
+                                          }}
+                                        />
+                                    )}</DataTable.Cell>
                                 </DataTable.Header>
 
                                 <DataTable.Row>
@@ -108,83 +93,7 @@ export default class ProfileView extends Component {
                         </View>
                     </React.Fragment>
                 </ScrollView>
-                {/* <Portal>
-                    <EditDialog 
-                        visible={this.state.editDialogVisible}
-                        onDismiss={() => { this.setState({editDialogVisible: false}); }}
-                        title={this.state.editDialogTitle}
-                        label={this.state.editDialogLabel}
-                        isNumeric={this.state.editDialogIsNumeric}
-                        initValues={() => {
-                            console.log(this.state.currEditedElements); // Logs empty array as soon as this component renders. 
-                                                                        // Doesn't update until EditDialog popup is closed
-                            return this.state.currEditedElements.map((elm, idx) => this.state.editedProfile[elm]);
-                        }}
-                        onSubmit={(newVals) => { 
-                            let updated = ld.cloneDeep(this.state.editedProfile);
-                            this.state.currEditedElements.forEach((elm, idx) => { updated[elm] = newVals[idx]; });
-                            this.setState({editedProfile: updated}); 
-                        }}
-                    />
-                </Portal> */}
             </React.Fragment>
         );
     }
 }
-
-// function EditDialog(props) {
-//     const [textboxValues, setText] = useState(props.initValues);
-
-//     function convertInputToNumber(input) {
-//         let numeric;
-//         return numeric;
-//     }
-
-//     return (
-//         <Dialog visible={props.visible} onDismiss={() => {props.onDismiss();}}>
-//             <Dialog.Title>{props.username}</Dialog.Title>
-//             <Dialog.Content>
-//                 {textboxValues.map((text, idx)=>{
-//                     return (
-//                         <TextInput 
-//                             key={idx}
-//                             label={props.label} 
-//                             mode="outlined" 
-//                             value={text === "" || text ? text : props.initValues[idx]} 
-//                             onChangeText={(newText) => { 
-//                                 let updated = ld.cloneDeep(textboxValues);
-//                                 updated[idx] = newText;
-//                                 setText(updated); 
-//                             }}
-//                         />
-//                     );
-//                 })}
-                
-//             </Dialog.Content>
-//             <Dialog.Actions>
-//                 <Button 
-//                     onPress={ () => { 
-//                         setText(props.initValues);
-//                         props.onDismiss(); 
-//                     }}
-//                 >CANCEL</Button>
-//                 <Button 
-//                     onPress={() => { 
-//                         props.isNumeric ? 
-//                             /* convert input value to number and execute callback */ null
-//                         :
-//                             props.onSubmit(textboxValues);
-//                         props.onDismiss();
-//                     }}
-//                 >SAVE</Button>
-//             </Dialog.Actions>
-//         </Dialog>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     image: {
-//         flexDirection: 'row',
-//         height: 150
-//     }
-// });
